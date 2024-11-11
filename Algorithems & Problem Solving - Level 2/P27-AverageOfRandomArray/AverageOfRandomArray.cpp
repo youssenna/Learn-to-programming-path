25123d9629434c7200524b4757eead0022930869
youssef43a <youssefennajar2@gmail.com>
2024-11-11 23:32:22 +0100

    add problem number 27 average of random array

---
+++ Algorithems & Problem Solving - Level 2/P27-AverageOfRandomArray/AverageOfRandomArray.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int ft_RandomNumber(int from, int to) {
+    int rendomNumber = rand() % (to - from + 1) + from;
+    return rendomNumber;
+}
+
+int ft_ReadPositifNumber(string message) {
+    int nbr = -1;
+    while (nbr <= 0 ) {
+        cout << message;
+        cin >> nbr;
+    }
+    return nbr;
+}
+
+void ft_FillArrayElements(int arrayElement[100], int length) {
+    int i = 0;
+    while (i < length) {
+        arrayElement[i] = ft_RandomNumber(1, 100);
+        i++;
+    }
+}
+
+void ft_printArray(int arry[100], int lenth) {
+    int i = 0;
+    cout << "\nArrat Elemment: ";
+    while (i < lenth) {
+        cout << arry[i] << " ";
+        i++;
+    }
+    cout << endl;
+}
+
+int ft_SumOfAllNumberArray(int array[100], int length) {
+    int sum = 0;
+    int i = 0;
+    while (i < length) {
+        sum += array[i];
+        i++;
+    }
+    return sum;
+}
+
+int ft_AverageofArray(int sum, int arraylength) {
+    return sum / arraylength;
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+    int array[100], length;
+    length = ft_ReadPositifNumber("Enter positf number: ");
+    
+    ft_FillArrayElements(array,length);
+    ft_printArray(array, length);
+    
+    cout << "\nAverage of all number is: " << ft_AverageofArray(ft_SumOfAllNumberArray(array, length),length) << endl;
+    return 0;
+}
