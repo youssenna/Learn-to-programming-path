0b9cf1df262428d300815b37b223ac1c2ad72548
youssef43a <youssefennajar2@gmail.com>
2024-11-15 11:10:41 +0100

    add Problem number 47 great my round fonction

---
+++ Algorithems & Problem Solving - Level 2/P47-MyRound/MyRound.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+# include <cmath>
+
+using namespace std;
+
+float ft_ReadNumber(string message) {
+    float nbr;
+    cout << message;
+    cin >> nbr;
+    return nbr;
+}
+
+float ft_MyABS(float nbr) {
+    if (nbr >= 0)
+        return nbr;
+    return nbr * -1;
+}
+
+float ft_MyRoundFunction(float nbr) {
+    if (nbr > 0){
+        if(nbr - (int)nbr < 0.5)
+            return int(nbr);    
+        else
+            return (int)nbr + 1;
+    }
+
+    else {
+        if(nbr - (int)nbr > -0.5)
+            return int(nbr);    
+        else
+            return (int)nbr - 1;
+    }
+}
+
+int main() {
+    float number;
+    number = ft_ReadNumber("enter a float number to see its round: ");
+    cout << "\n\nMy Round Resoult : " << ft_MyRoundFunction(number); 
+    cout << "\n\nC++ Round Resoult: " << round(number) << endl; 
+}
